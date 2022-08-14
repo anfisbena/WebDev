288d3e61b854c4450ef39714d6dfe2bad04d3575
Andres Arcila <arcilacarmona@gmail.com>
2022-08-14 15:55:43 -0500

    ff

---
+++ TESTEO_VIOLENTO/script/script.js
@@ -0,0 +1,7 @@
+let answer='EL MASCAPORONGAS!!';
+let number=420;
+let booleano=false;
+let name=prompt('decime tu nombre guachin!');
+
+alert('Gracias!');
+document.write('Che!, '+name+' '+answer+' '+number+' mas '+booleano+' que un hptas.');
