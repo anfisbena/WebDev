02cf7c3cdd316f5aed0e6b9081446bd8d9c44ff0
Andres Arcila <arcilacarmona@gmail.com>
2022-09-19 21:04:11 -0500

    tt

--- js/scripts.js
+++ js/scripts.js
@@ -5,51 +5,46 @@ const trufas=[
   {
     id:'balones',
     valor:800,
-    cantidad:0,
   },
   {
     id:'chocoratas',
     valor:300,
-    cantidad:0,
   },
   {
     id:'corazones1',
     valor:400,
-    cantidad:0,
   },
   {
     id:'corazones3',
     valor:700,
-    cantidad:0,
   },
   {
     id:'tulipan',
     valor:600,
-    cantidad:0,
   },
   {
     id:'manzanilla',
     valor:600,
-    cantidad:0,
   },
   {
     id:'orquidea',
     valor:900,
-    cantidad:0,
   },
 ];
 
 //funcion para agregar items a la lista
+function agregarItem(){
+  document.getElementById('formTrufa').onsubmit=(e)=>{
+    e.preventDefault();
+    Array.from(e.target[1]).forEach(element => {
+      if(element.selected){
+        console.log(element.value)
+        let indice=trufas.findIndex((item)=>item.id===element.value);
+        sessionStorage.setItem(trufas[indice].id,JSON.stringify(trufas[indice].valor));
+        sessionStorage.setItem(trufas[indice].id,JSON.stringify(trufas[indice].cantidad=e.target[2].value));
+      }
+    })
+  }; 
+}
 
-document.getElementById('formTrufa').onsubmit=(e)=>{
-  Array.from(e.target[1]).forEach(element => {
-    if(element.selected==true){
-      objectPos=trufas.findIndex((item)=>item.id===element.value);
-      trufas[objectPos].cantidad=parseInt(e.target[2].value);
-    }
-  })
-  // sessionStorage.setItem('trufas',JSON.stringify(trufas));
-};
-  
-console.log(trufas.forEach((item)=>{parseInt(item.cantidad)*parseInt(item.valor);}));
-document.querySelector('.subtotal').innerHTML=`$${subtotal}`;
+agregarItem();
