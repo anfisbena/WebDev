eaace9d8769f0153ca7547d6d27c43a84340cb1a
Andres Arcila <arcilacarmona@gmail.com>
2022-10-08 13:56:48 -0500

    ss

--- js/checkOut.js
+++ js/checkOut.js
@@ -1,26 +1,14 @@
 class usuario{
 
-  // muestraCheckOut(){
-  //   let carrito=JSON.parse(sessionStorage.carrito);
-  //   let total=0;
-  //   for(let i=1;i<sessionStorage.length;i++){
-  //     let data=JSON.parse(sessionStorage.getItem(sessionStorage.key(i)));
-  //     document.querySelector('.list-group-item.d-flex.justify-content-between.listaItems').insertAdjacentHTML("beforebegin",
-  //     `<li class="list-group-item d-flex justify-content-between lh-condensed">
-  //         <div>
-  //           <h6 class="my-0 ">${data.id}</h6>
-  //           <small class="text-muted">${data.cantidad} UDS, a $${data.valorUD} C/U</small>
-  //         </div>
-  //         <span class="text-muted">$${data.cantidad*data.valorUD}</span>
-  //     </li>`);
-      
-  //     total=total+data.cantidad*data.valorUD;
-  //   }
-  //   document.getElementById('Total').innerHTML=`$${total}`;
-  // }
-  
   muestraCheckOut(){
-    let carrito=JSON.parse(sessionStorage.carrito);
+    let carrito=sessionStorage.carrito?
+      JSON.parse(sessionStorage.carrito):
+      Swal.fire({
+        title: 'Alerta!',
+        text: 'no tienes items en tu carrito',
+        icon: 'warning',
+        confirmButtonText: 'Cool'
+      }).then(()=>{window.history.go(-1)});
     let total=0;
     carrito.forEach(element=>{
       document.querySelector('.list-group-item.d-flex.justify-content-between.listaItems').insertAdjacentHTML("beforebegin",
@@ -45,7 +33,7 @@ class usuario{
         text: 'Esperamos que disfrutes de tu compra',
         icon: 'success',
         confirmButtonText: 'Cool'
-      })
+      }).then(()=>{window.history.go(-1)})
     }
     sessionStorage.clear();
   }
