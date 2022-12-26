8c2cb5b3ce407d692d0fa82218e7fcf60ebac53e
Andres Arcila <arcilacarmona@gmail.com>
2022-12-26 18:33:38 -0500

    sss

---
+++ coderhouse/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return(
+    <>
+      <h1>App</h1>
+      <NavBar />
+    </>
+  )
+}
+
+export default App;
---
+++ coderhouse/src/components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <Nav>
+      <NavLink to='/'></NavLink>
+    </Nav>
+  )
+}
+
+export default NavBar
