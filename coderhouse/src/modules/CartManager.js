3e49599cd3207a16a84fd664ceab8a18b34904e2
Andres Arcila <arcilacarmona@gmail.com>
2023-03-18 19:49:18 -0500

    gg

--- coderhouse/src/modules/CartManager.js
+++ coderhouse/src/modules/CartManager.js
@@ -1,8 +1,8 @@
 /*You can easily read my code like this:
 function name(){
+  //Core conditionals
   //Variables
   result = Final result
-
   try{result}
   catch{error}
 }
@@ -18,17 +18,23 @@ export default class CartManager{
   }
   
   getCart=async()=>{
+//VARIABLES
     const getCart=await fs.promises.readFile(this.path,'utf-8')
+//RESULT
     const result=JSON.parse(getCart)
-
     try{return result}
     catch(err){console.log(err)}
   }
 
   addProduct=async(cid,pid)=>{
+//CORE
+    if(!cid||!pid){return 'error'}
+//VARIABLES
     this.cartList=await this.getCart()
     let cart=this.cartList.findIndex(cart=>cart.id===cid)
     let newCid=this.cartList.length===0?1:this.cartList[this.cartList.length-1].id+1
+    let productList=this.cartList[cart].products
+    let productIndex=productList.findIndex(product=>product.product===pid)
     let newCart=
     { 
       id:newCid,
@@ -39,25 +45,16 @@ export default class CartManager{
         }
       ]
     }
-    
-    if(!cid||!pid){
-      return 'error'
-    }
-    if(cart===-1){
-      this.cartList=[...this.cartList,newCart]
-      
-    }
+//MAGIC
+    if(cart===-1){this.cartList=[...this.cartList,newCart]}
     else{
-      let productList=this.cartList[cart].products
-      let productIndex=productList.findIndex(product=>product.product===pid)
-
       productIndex===-1
         ?productList.push({product:pid,quantity:1})
         :productList[productIndex].quantity++;
-
-      const result=await fs.promises.writeFile(this.path,JSON.stringify(this.cartList),(err,data)=>err??data)
-      try{return result}
-      catch(err){ console.log(err)}
     }
+//RESULT    
+    const result=await fs.promises.writeFile(this.path,JSON.stringify(this.cartList),(err,data)=>err??data)
+    try{return result}
+    catch(err){ console.log(err)}
   }
 }
