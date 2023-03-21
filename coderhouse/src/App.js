9b0f0476c7d5cad94f71fc3c4469a4ec909ee018
Andres Arcila <arcilacarmona@gmail.com>
2023-03-20 23:38:01 -0500

    wss

--- coderhouse/src/App.js
+++ coderhouse/src/App.js
@@ -1,6 +1,7 @@
 //quede 1:35'
 import express from 'express';
 import homeRouter from './routes/home.router.js'
+import realTimeProducts from './routes/realtimeproducts.router.js'
 import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
 import {engine} from 'express-handlebars';
@@ -9,24 +10,19 @@ import __dirname from './utils.js';
 //Declaracion de variables Express
 const app=express()
 const PUERTO=8080;
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
-//declaracion de folder public
-app.use(express.static(`${__dirname}/public`));
-
-app.use("/api/products",productRouter)
-app.use("/api/carts",cartRouter)
 
 //Configuracion de handlebars
 app.engine('handlebars',engine())
 app.set('views',`${__dirname}/views`)
 app.set('view engine','handlebars')
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(`${__dirname}/public`)); //declaracion de folder public
 app.use('/',homeRouter)
+app.use("/api/products",productRouter)
+app.use("/api/carts",cartRouter)
 
-app.get('/realtimeproducts',(req,res)=>{
-  res.render('realtimeproducts')
-})
 
 
 
--- coderhouse/src/routes/realtimeproducts.router.js
+++ coderhouse/src/routes/realtimeproducts.router.js
@@ -5,16 +5,11 @@ const router = Router();
 const ProdMan=new ProductManager('./src/database/productos.json')
 
 //GET
-router.get('/',async(req,res)=>{
+router.get('/realtimeproducts',async(req,res)=>{
   const productos=await ProdMan.getProducts()
   let limit=req.query.limit;
   let productLimit=productos.slice(0,limit||productos.length)
-  return res.render('home',
-  {
-    title:'Home',
-    product:productLimit,
-    css:'/css/styles.css'
-  })
+  return res.render('realtimeproducts')
 })
 
 
