48f642388ac9745364128277515e0382ee2423b2
Andres Arcila <arcilacarmona@gmail.com>
2023-03-26 16:27:09 -0500

    ee

--- coderhouse/src/public/js/realTimeProducts.js
+++ coderhouse/src/public/js/realTimeProducts.js
@@ -1,18 +1,23 @@
 
 const socket=io();
 socket.emit("message","Cliente conectado a socket")
-socket.on('realTimeProducts',async(newProduct)=>{
-  const imageList=newProduct.thumbnails.forEach(element =>`<img src="${element}" alt="productItem">`)
+socket.on('realTimeProducts',(newProduct)=>{
   const OldItemList=document.getElementById('itemList');
-  const NewItemList=document.createElement('div');
-  NewItemList.innerHTML=(
+  const ImageList=newProduct.thumbnails.forEach(element =>`<img src="${element}" alt="productItem">`)
+  console.log(ImageList)
+  const NewItem=document.createElement('div')
+  NewItem.className='item'
+
+  NewItem.innerHTML=(
     `
-    ${imageList}
     <h2>${newProduct.title}</h2>
     <p>NKU:${newProduct.code}</p>
     <p>${newProduct.description}</p>
     <h2>${newProduct.price}</h2>
-  `)
+    `
+  )
+  
 
-  return OldItemList.appendChild(NewItemList);
+  return OldItemList.appendChild(NewItem);
+  
 })
